2c69fdcc7810a57fc0658c3610d1ad991dbd75c0
fredsalm <salmsimplon@gmail.com>
2018-01-09 16:59:09 +0100

    finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 (function main(){
+	function horlogeMaj(){
+
 	var date = new Date();
 	var heure = date. getHours();
 	var minutes = date. getMinutes();
+	var sec = date.getSeconds();
+
+	$('#heure').css({'transform':'rotate('+(heure*30)+'deg)'})
+	$('#minute').css({'transform':'rotate('+(minutes*6)+'deg)'});
+	$('#sec').css({'transform':'rotate('+(sec*6)+'deg)'});
+	}
+	
+
+	setInterval(horlogeMaj,500);
+
+	
 }());
